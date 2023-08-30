dca4f480489a1e5ec0792d57a12e43999a827055
LiuSantana <liucs02@gmail.com>
2023-08-31 00:26:22 +0200

    https

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express"); 
 const https = require('https');
+const fs = require('fs');
 
 const options = {
   key: fs.readFileSync('/etc/letsencrypt/live/mirrorcup.org/privkey.pem'),
