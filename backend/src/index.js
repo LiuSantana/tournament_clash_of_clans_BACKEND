8c06b2311e307d9138caba878dbea10c8c152e66
LiuSantana <liucs02@gmail.com>
2023-08-31 22:55:43 +0200

    https & cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express"); 
 const https = require('https');
 const fs = require('fs');
+const cors = require('cors'); // Mueve la importación aquí
 
 const options = {
   key: fs.readFileSync('/etc/letsencrypt/live/mirrorcup.org/privkey.pem'),
@@ -8,18 +9,18 @@ const options = {
 };
 
 const app = express(); 
-const cors = require('cors');
 
 const PORT = process.env.PORT || 3000; 
-
 
 const v1Router = require("./v0.1/routes");
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
-app.use("/api/v0.1", v1Router);
 
-app.use(cors());
+// Configura CORS antes de configurar las rutas
+app.use(cors({origin: '*'}));
+
+app.use("/api/v0.1", v1Router);
 app.use(express.json());
 
 const server = https.createServer(options, app);
@@ -30,4 +31,3 @@ server.listen(PORT, () => {
 server.on('error', error => {
   console.error('Error starting HTTPS server:', error);
 });
-
